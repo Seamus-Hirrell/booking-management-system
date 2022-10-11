f78c474234c78cc9da767f80965af0e278f32c01
Séamus Hirrell <seamushirrell@gmail.com>
2022-10-12 00:02:25 +0100

    fixed bug

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,13 @@
 import { $ } from '@builder.io/qwik';
 import { Client, Account, ID } from 'appwrite';
 
-export const client = new Client()
-  .setEndpoint('https://localhost/v1') // Your API Endpoint
-  .setProject('632cce4e4acb6c10a605'); // Your project ID
-
-export const account = new Account(client);
-
 // Register User
 export const registerUser = $(
   (email: string, password: string, name: string) => {
+    const client = new Client()
+      .setEndpoint('https://localhost/v1') // Your API Endpoint
+      .setProject('632cce4e4acb6c10a605'); // Your project ID
+    const account = new Account(client);
     account.create(ID.unique(), email, password, name).then(
       (response) => {
         console.log(response);
