5d4cc6809d133c7da925015181bd575a24a2107d
Séamus Hirrell <seamushirrell@gmail.com>
2023-02-14 22:08:22 +0000

    added teams to api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,12 @@
 import { $ } from '@builder.io/qwik';
-import { Client, Account, ID, Databases } from 'appwrite';
+import { Client, Account, ID, Databases, Teams } from 'appwrite';
 
 const client = new Client()
   .setEndpoint('https://appwrite.xn--samushirrell-beb.live/v1')
   .setProject('63b0a101b617044131c3');
 export const account = new Account(client);
 export const databases = new Databases(client);
+export const teams = new Teams(client);
 
 // Register User
 export const registerUser = $(
