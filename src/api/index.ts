3807d1453884ce9b2520caba37f55585102e69a5
Séamus Hirrell <seamushirrell@gmail.com>
2023-04-26 20:52:17 +0100

    removed old getUser function in api.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,25 +1,9 @@
-import { $ } from '@builder.io/qwik';
 import { Client, Account, Databases, Teams } from 'appwrite';
 
 const client = new Client()
   .setEndpoint('https://appwrite.xn--samushirrell-beb.live/v1')
   .setProject('63b0a101b617044131c3');
+
 export const account = new Account(client);
 export const databases = new Databases(client);
 export const teams = new Teams(client);
-
-
-
-
-
-// Get User
-export const getUser = $(() => {
-  account.get().then(
-    (response) => {
-      console.log(response);
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
-});
